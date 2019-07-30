e272525751d7163464f3559c8052bb7be7183b4f
tarnishablec <tarnishablec@outlook.com>
2019-07-30 16:36:14 +0800

    travis ci

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const resolve = dir => require('path').join(__dirname, dir);
+
+module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/fortjoy-shell/'
+		: '/',
+	chainWebpack: (config) => {
+		config.resolve.alias
+			.set('@', resolve('src'))
+	},
+
+	devServer: {
+		port: 9874,
+
+	},
+	runtimeCompiler: true
+};
